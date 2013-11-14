53a0dc25102d162ede58036136aa7e136d2b4cef
vircures <vircures+github@gmail.com>
2013-11-13 22:35:33 -0500

    Adding hlint to travis

---
+++ travis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+rm -f dist/*.tar.gz
+rm -rf dist/snapshot
+cabal configure --ghc-options=-Werror
+cabal sdist
+cd dist
+mkdir snapshot
+tar -xf *.tar.gz -C snapshot
+cd snapshot
+mv hlint-* hlint # predictable name
+cd hlint
+cabal configure --ghc-options=-Werror
+cabal build
+cd ../../..
+hlint_datadir=data dist/snapshot/hlint/dist/build/hlint/hlint --test
